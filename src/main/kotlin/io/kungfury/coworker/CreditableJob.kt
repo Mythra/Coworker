efab84c6fb58be64055b6cff61920f2071d408c9
Cynthia Coan <ccoan@instructure.com>
2019-07-14 10:56:28 -0600

    make creditable job actually work
    * make it take into account job stages
    * handle unlogged table going away and subtracting more than it has
      "in_use".
    * check credit after it's scheduled in case two people schedule at
      the same time causing it to go over.

--- src/main/kotlin/io/kungfury/coworker/CreditableJob.kt
+++ src/main/kotlin/io/kungfury/coworker/CreditableJob.kt
@@ -21,67 +21,124 @@ import kotlin.system.measureTimeMillis
 @UseExperimental(CreditApi::class)
 abstract class CreditableJob(
     private val connectionManager: ConnectionManager,
+    private val workUniqueName: String,
     garbageHeap: WorkGarbage,
     id: Long,
     stage: Int,
     strand: String,
-    priority: Int,
-    private val workUniqueName: String
+    priority: Int
 ) : BackgroundKotlinWork(garbageHeap, id, stage, strand, priority) {
+    // TODO: Make rolling window.
+    private val MAX_SECONDS_WORKABLE: Long = 3600
+
+    private suspend fun GetWorkCredit(): CreditableJobState {
+        return when (connectionManager.CONNECTION_TYPE) {
+            ConnectionType.POSTGRES -> {
+                connectionManager.executeTransaction { conn ->
+                    val statement = conn.prepareStatement(Marginalia.AddMarginalia(
+                        "CreditableJob_SelectCredits",
+                        "SELECT" +
+                            "  rolling_average_seconds," +
+                            "  total_jobs" +
+                            " FROM" +
+                            "  public.delayed_work_credits" +
+                            " WHERE" +
+                            "  strand_name = ?" +
+                            " AND" +
+                            "  stage = ?"
+                    ))
+                    statement.setString(1, Strand)
+                    statement.setInt(2, Stage)
+
+                    val rs = statement.executeQuery()
+                    var rolling_avg: Long = 0
+                    var total_jobs: Long = 0
+
+                    if (rs.next()) {
+                        rolling_avg = rs.getLong("rolling_average_seconds")
+                        total_jobs = rs.getLong("total_jobs")
+                    }
+
+                    var in_use: Long = 0
+                    val credit_use = conn.prepareStatement(Marginalia.AddMarginalia(
+                        "CreditableJob_InUseCount",
+                        "SELECT" +
+                            "  in_use" +
+                            " FROM" +
+                            "  public.delayed_work_credit_use" +
+                            " WHERE" +
+                            "  strand_name = ?" +
+                            " AND" +
+                            "  stage = ?"
+                    ))
+                    credit_use.setString(1, Strand)
+                    credit_use.setInt(2, Stage)
+
+                    val credit_use_rs = credit_use.executeQuery()
+                    if (rs.next()) {
+                        in_use = rs.getLong("in_use")
+                    }
+
+                    return@executeTransaction CreditableJobState(rolling_avg, total_jobs, in_use)
+                }
+            }
+        }
+    }
+
+    private suspend fun UpdateCredits(rolling_avg: Long, newAvg: Long, newJobCount: Long) {
+        when (connectionManager.CONNECTION_TYPE) {
+            ConnectionType.POSTGRES -> {
+                connectionManager.executeTransaction { conn ->
+                    val update_in_use_ps = conn.prepareStatement(Marginalia.AddMarginalia(
+                        "CreditableJob_UpdateInUse",
+                        "UPDATE" +
+                            "  public.delayed_work_credit_use" +
+                            " SET" +
+                            "  in_use = GREATEST(delayed_work_credit_use.in_use - ?, 0)" +
+                            " WHERE" +
+                            "  strand_name = ?" +
+                            " AND" +
+                            "  stage = ?"
+                    ))
+                    update_in_use_ps.setLong(1, rolling_avg)
+                    update_in_use_ps.setString(2, Strand)
+                    update_in_use_ps.setInt(3, Stage)
+
+                    update_in_use_ps.execute()
+
+                    val update_avg = conn.prepareStatement(Marginalia.AddMarginalia(
+                        "CreditableJob_UpdateAvgs",
+                        "INSERT INTO" +
+                            "  public.delayed_work_credits(strand_name, stage, rolling_average_seconds, total_jobs)" +
+                            " VALUES" +
+                            "  (?, ?, ?, ?)" +
+                            " ON CONFLICT(strand_name, stage)" +
+                            " DO UPDATE SET" +
+                            "  rolling_average_seconds = ?," +
+                            "  total_jobs = ?"
+                    ))
+                    update_avg.setString(1, Strand)
+                    update_avg.setInt(2, Stage)
+                    update_avg.setLong(3, newAvg)
+                    update_avg.setLong(4, newJobCount)
+                    update_avg.setLong(5, newAvg)
+                    update_avg.setLong(6, newJobCount)
+                    update_avg.execute()
+                }
+            }
+        }
+    }
+
     override fun WorkPart(state: String, coroutineContext: CoroutineContext): Job {
         return CoroutineScope(coroutineContext).launch {
             if (Strand.isEmpty()) {
                 return@launch failWork(connectionManager, workUniqueName, "Creditable Jobs need a strand!")
             }
 
-            val jobState = when (connectionManager.CONNECTION_TYPE) {
-                ConnectionType.POSTGRES -> {
-                    connectionManager.executeTransaction { conn ->
-                        val statement = conn.prepareStatement(Marginalia.AddMarginalia(
-                            "CreditableJob_SelectCredits",
-                            "SELECT" +
-                                "  rolling_average_seconds," +
-                                "  total_jobs" +
-                                " FROM" +
-                                "  public.delayed_work_credits" +
-                                " WHERE" +
-                                "  strand_name = ?"
-                        ))
-                        statement.setString(1, Strand)
-
-                        val rs = statement.executeQuery()
-                        var rolling_avg: Long = 0
-                        var total_jobs: Long = 0
-
-                        if (rs.next()) {
-                            rolling_avg = rs.getLong("rolling_average_seconds")
-                            total_jobs = rs.getLong("total_jobs")
-                        }
-
-                        var in_use: Long = 0
-                        val credit_use = conn.prepareStatement(Marginalia.AddMarginalia(
-                            "CreditableJob_InUseCount",
-                            "SELECT" +
-                                "  in_use" +
-                                " FROM" +
-                                "  public.delayed_work_credit_use" +
-                                " WHERE" +
-                                "  strand_name = ?"
-                        ))
-                        credit_use.setString(1, Strand)
-
-                        val credit_use_rs = credit_use.executeQuery()
-                        if (rs.next()) {
-                            in_use = rs.getLong("in_use")
-                        }
-
-                        return@executeTransaction CreditableJobState(rolling_avg, total_jobs, in_use)
-                    }
-                }
-            }
+            val jobState = GetWorkCredit()
 
             // If a job takes longer than an hour let it run one job.
-            if (jobState.in_use + jobState.rolling_avg > 3600 && jobState.in_use != 0L) {
+            if (jobState.in_use + jobState.rolling_avg > MAX_SECONDS_WORKABLE && jobState.in_use != 0L) {
                 return@launch yieldCurrentStage(connectionManager, Instant.now().plusSeconds(60))
             }
 
@@ -107,15 +164,17 @@ abstract class CreditableJob(
                 }
             }
 
-            // TODO: recheck to see if we caused an overflow.
-
-            val time = measureTimeMillis {
-                Work(state)
+            val newState = GetWorkCredit()
+            if (newState.in_use > MAX_SECONDS_WORKABLE) {
+                UpdateCredits(jobState.rolling_avg, jobState.rolling_avg, jobState.total_jobs)
+                return@launch yieldCurrentStage(connectionManager, Instant.now().plusSeconds(60))
             }
+
+            val time = measureTimeMillis { Work(state) }
 
             val newJobCount: Long = jobState.total_jobs + 1
 
-            var newAvg: Long = if (jobState.rolling_avg == 0L) {
+            val newAvg: Long = if (jobState.rolling_avg == 0L) {
                 Math.floorDiv(time, 1000)
             } else {
                 var new_avg = jobState.rolling_avg - (jobState.rolling_avg / newJobCount)
@@ -123,40 +182,7 @@ abstract class CreditableJob(
                 new_avg
             }
 
-            when (connectionManager.CONNECTION_TYPE) {
-                ConnectionType.POSTGRES -> {
-                    connectionManager.executeTransaction { conn ->
-                        val update_in_use_ps = conn.prepareStatement(Marginalia.AddMarginalia(
-                            "CreditableJob_UpdateInUse",
-                            "UPDATE" +
-                                "  public.delayed_work_credit_use" +
-                                " SET" +
-                                "  in_use = delayed_work_credit_use.in_use - ?"
-                        ))
-                        update_in_use_ps.setLong(1, jobState.rolling_avg)
-
-                        update_in_use_ps.execute()
-
-                        val update_avg = conn.prepareStatement(Marginalia.AddMarginalia(
-                            "CreditableJob_UpdateAvgs",
-                            "INSERT INTO" +
-                                "  public.delayed_work_credits(strand_name, rolling_average_seconds, total_jobs)" +
-                                " VALUES" +
-                                "  (?, ?, ?)" +
-                                " ON CONFLICT(strand_name)" +
-                                " DO UPDATE SET" +
-                                "  rolling_average_seconds = ?," +
-                                "  total_jobs = ?"
-                        ))
-                        update_avg.setString(1, Strand)
-                        update_avg.setLong(2, newAvg)
-                        update_avg.setLong(3, newJobCount)
-                        update_avg.setLong(4, newAvg)
-                        update_avg.setLong(5, newJobCount)
-                        update_avg.execute()
-                    }
-                }
-            }
+            UpdateCredits(jobState.rolling_avg, newAvg, newJobCount)
         }
     }
 }
--- src/main/resources/migrations/2_CreateJobCredit_pg.sql
+++ src/main/resources/migrations/2_CreateJobCredit_pg.sql
@@ -1,10 +1,14 @@
 CREATE TABLE IF NOT EXISTS public.delayed_work_credits (
-    strand_name VARCHAR(255) PRIMARY KEY,
+    strand_name VARCHAR(255),
+    stage INTEGER,
     rolling_average_seconds BIGINT,
-    total_jobs BIGINT
+    total_jobs BIGINT,
+    PRIMARY KEY(strand_name, stage)
 );
 
 CREATE UNLOGGED TABLE IF NOT EXISTS public.delayed_work_credit_use(
-    strand_name VARCHAR(255) PRIMARY KEY,
-    in_use BIGINT
+    strand_name VARCHAR(255),
+    stage INTEGER,
+    in_use BIGINT,
+    PRIMARY KEY(strand_name, stage)
 );
